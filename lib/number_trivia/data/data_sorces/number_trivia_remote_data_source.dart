12d11d3cec8f148493c41e765fa73ab60a23f6c3
sergeik <sergey.r.karimov@hotmail.com>
2022-11-24 23:15:57 +0200

    Add get random number trivia in remote data source implementation

--- lib/number_trivia/data/data_sorces/number_trivia_remote_data_source.dart
+++ lib/number_trivia/data/data_sorces/number_trivia_remote_data_source.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:number_trivia/core/errors/exceptions.dart';
 
@@ -32,8 +31,16 @@ class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
   }
 
   @override
-  Future<NumberTriviaModel> getRandomNumberTrivia() {
-    // TODO: implement getRandomNumberTrivia
-    throw UnimplementedError();
+  Future<NumberTriviaModel> getRandomNumberTrivia() async {
+    final response = await client.get(
+      Uri(scheme: 'http', host: 'numbersapi.com', path: 'random'),
+      headers: {'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
   }
 }
